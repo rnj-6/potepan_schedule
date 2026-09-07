64f819c0195ec6281d4217f276c6513499bd46c7
河内妃奈子 <hina.k0310q@gmail.com>
2026-09-07 23:46:11 +0900

    バリデーションの機能を実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ApplicationRecord
+  validates :title,
+      presence: true,
+      length: { maximum: 20 }
+
+  validates :start_date, presence: true
+
+  validates :end_date,
+      presence: true
+
+  validate :dates_are_valid
+
+  validates :body, length: { maximum: 500 }
+
+  private
+
+  def dates_are_valid
+    return if start_date.blank? || end_date.blank?
+    if end_date < start_date
+      errors.add(:end_date, "は開始日以降の日付を選択してください")
+    end
+  end
 end
